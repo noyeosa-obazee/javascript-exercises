6bdacb66e2c03e713c056bd4ded44110a27e8d91
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-07 11:49:29 +0100

    Do sum all javascript exercise and run all tests

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0) {
+        if (a < b) {
+            const rangeArray = [];
+            for (a; a <= b; a++) rangeArray.push(a);
+            return rangeArray.reduce((sum, n) => sum + n);
+        }
+
+        else {
+            const rangeArray = [];
+            for (b; b <= a; b++) rangeArray.push(b);
+            return rangeArray.reduce((sum, n) => sum + n);
+        }
+
+    }
+
+    else return 'ERROR';
 
 };
 
