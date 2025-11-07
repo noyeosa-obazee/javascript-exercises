07f34b026ce51717ed0f708939a360c3bf951ca8
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-07 12:55:16 +0100

    Do temperature conversion exercise(f to c and c to f) and run all tests

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+  let result;
+  const convertedValue = (value - 32) * (5/9);
+  result = Math.round(convertedValue * 10) / 10;
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let result;
+  const convertedValue = (value * (9/5)) + 32;
+  result = Math.round(convertedValue * 10) / 10;
+  return result;
+
 };
 
 // Do not edit below this line
