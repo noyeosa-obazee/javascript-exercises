2e47d9c81c2d6fa52c948031dac0c6841ed7be94
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-07 11:02:41 +0100

    Do repeat string exercise and run all tests

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+
+    if (num >= 0) {
+    let buildingString = '';
+
+    for(let i = 0; i < num; i++) buildingString+=string
+
+    return buildingString;
+    }
+
+    else return 'ERROR';
 
 };
 
