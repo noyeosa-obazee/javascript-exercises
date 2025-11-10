6cd913e6ff17418b79215228d1ed793f67d91f29
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-10 12:12:28 +0100

    Do exercise on palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const original = word.toLowerCase().split('').filter((element) => !/[^\w\s]/.test(element) && element !== ' ');
+    const reversed = [...original].reverse();
+    return original.join('') === reversed.join('');
 };
 
 // Do not edit below this line
