314ea8315164f736c9741420f73b91e85adeca0d
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-10 14:12:27 +0100

    Do fibonacci sequence exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+const fibonacci = function(memberNumber) {
+    const n = parseInt(memberNumber);
+    if (n < 0) return 'OOPS';
+    const noOfFibonaccis = 45;
+    const fibArr = [0,1];
+    for (let i = 1; i < noOfFibonaccis; i++) {
+        fibArr.push(fibArr[fibArr.length-1] + fibArr[fibArr.length - 2]);
+    }
+    return fibArr[n];
 };
 
 // Do not edit below this line
