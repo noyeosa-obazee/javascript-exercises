8c2de4623a39c93a474fe1d6bcb1acdde4ca600a
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-10 14:41:14 +0100

    Do find the oldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(arr) {
+    return arr.sort( function (lastPerson, nextPerson) { 
+        const currentYear = new Date().getFullYear();
+        if (!lastPerson.yearOfDeath) lastPerson.yearOfDeath = currentYear;
+        if (!nextPerson.yearOfDeath) nextPerson.yearOfDeath = currentYear;
+        return (nextPerson.yearOfDeath - nextPerson.yearOfBirth) - (lastPerson.yearOfDeath - lastPerson.yearOfBirth);
+    })[0]};
 
 // Do not edit below this line
 module.exports = findTheOldest;
