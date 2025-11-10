8fd23d661fc23f7dae91bbcb9897bde7e12dc09a
noyeosa-obazee <obazeenoyeosa@gmail.com>
2025-11-10 11:09:13 +0100

    Do calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, number) => sum + number, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((multiplication, number) => multiplication * number, 1);
 
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	const factorialNumbers = [];
+  for(let i = 1; i <= a; i++) factorialNumbers.push(i);
+  return factorialNumbers.reduce((multiplication, number) => multiplication * number, 1)
 };
 
 // Do not edit below this line
